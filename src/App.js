79709f66fc60e906bbb0abc29a67cf9851eda7f3
wanganna <wanganna@uw.edu>
2022-03-24 01:42:45 -0700

    Remove bullet points next to list items, display number of incomplete tasks, add button to clear all tasks

--- src/App.js
+++ src/App.js
@@ -4,26 +4,42 @@ import uniqid from 'uniqid';
 
 function App() {
   const [list, setList] = useState([]);
+  const [message, setMessage] = useState('No tasks added!');
 
   const addTask = () => {
     let textField = document.getElementById('text-field');
     let task = textField.value;
     textField.value = '';
-    let item = { id: uniqid(), text: task, completed: false };
-    list.push(item);
-    setList([...list]); // spread to new array so it renders again
+    if (task !== '') {
+      let item = { id: uniqid(), text: task, completed: false };
+      list.push(item);
+      setList([...list]); // spread to new array so it renders again
+      let length = list.filter((task) => !task.completed).length;
+      let message = length + ' incomplete task';
+      if (length !== 1) {
+        message += 's';
+      }
+      setMessage(message);
+    }
+  };
+
+  const clearAll = () => {
+    setList([]);
+    setMessage('No tasks added!');
   };
 
   return (
     <main>
       <section className='container'>
         <h1>Tasks</h1>
-        <List tasks={list} />
+        <List tasks={list} setMessage={setMessage} />
+        <p>{message}</p>
       </section>
       <section className='form'>
         <input type='text' id='text-field' />
         <input type='submit' onClick={addTask} />
       </section>
+      <button onClick={clearAll}>Clear All</button>
     </main>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,10 +1,28 @@
-function List({ tasks }) {
+function List({ tasks, setMessage }) {
+  const completeTask = (task) => {
+    task.completed = !task.completed;
+    let length = tasks.filter((task) => !task.completed).length;
+    let message = length + ' incomplete task';
+    if (length !== 1) {
+      message += 's';
+    }
+    setMessage(message);
+  };
+
   return (
     <ul className='list'>
-      {tasks.map(({ id, text, completed }) => {
+      {tasks.map((task) => {
+        const { id, text, completed } = task;
         return (
           <li key={id}>
-            <input id={id} type='checkbox' defaultChecked={completed} />
+            <input
+              id={id}
+              type='checkbox'
+              defaultChecked={completed}
+              onClick={() => {
+                completeTask(task);
+              }}
+            />
             <label htmlFor={id}>{text}</label>
           </li>
         );
